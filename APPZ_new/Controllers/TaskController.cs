9fcbaeb349ed46a9ef30a4a8478018b173c7125a
vasyl <60632175+vasylko73@users.noreply.github.com>
2021-12-05 20:40:56 +0200

    Adding delete and update to TaskController

--- APPZ_new/Controllers/TaskController.cs
+++ APPZ_new/Controllers/TaskController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using APPZ_new.Data;
+using APPZ_new.Enums;
 using Task = APPZ_new.Models.Task;
 
 namespace APPZ_new.Controllers
@@ -36,5 +37,66 @@ namespace APPZ_new.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteTask(int? id)
+        {
+            var obj = _db.Tasks.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Tasks.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Tasks.Find(id);
+            ViewBag.Severity = obj.Severity;
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Tasks.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Category = _db.Categorys.ToList();
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Task obj)
+        {
+            if (ModelState.IsValid)//то не обовязкове
+            {
+                _db.Tasks.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);//нахуй можна забрати
+        }
     }
 }
+
