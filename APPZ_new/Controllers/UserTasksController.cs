6045d969185cd82d7861347051d99e6d9c4867a4
vasyl <60632175+vasylko73@users.noreply.github.com>
2021-12-08 23:18:57 +0200

    addingPassTest

--- APPZ_new/Controllers/UserTasksController.cs
+++ APPZ_new/Controllers/UserTasksController.cs
@@ -10,6 +10,7 @@ using APPZ_new.Models;
 using APPZ_new.DTOs;
 using Microsoft.AspNetCore.Http;
 using APPZ_new.Enums;
+using Microsoft.CodeAnalysis.CSharp;
 using TaskStatus = APPZ_new.Enums.TaskStatus;
 
 namespace APPZ_new.Controllers
@@ -24,10 +25,12 @@ namespace APPZ_new.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> StartTask()
+        public async Task<IActionResult> StartTask(int id)
         {
-            var taskId = 3;
-            var userId = 1;
+            string currentUserName = User.Identity.Name;
+            IEnumerable<Models.User> user = _context.Users;
+            var taskId = id;
+            var userId = user.Where(q => q.Name == currentUserName).Select(p => p.Id).FirstOrDefault();
             var task = await _context.Tasks
                 .Include(x => x.Questions)
                 .ThenInclude(x => x.Answers)
@@ -48,7 +51,8 @@ namespace APPZ_new.Controllers
 
         public async Task<ActionResult> CompleteTask(IFormCollection form)
         {
-            var userId = int.Parse(form["UserId"]);
+            string currentUserName = User.Identity.Name;
+            var userId = _context.Users.Where(q => q.Name == currentUserName).Select(p => p.Id).FirstOrDefault();
             var taskId = int.Parse(form["TaskId"]);
             string[] answerIdsStr = form["item.AnswerId"];
             var answerIds = Array.ConvertAll(answerIdsStr, id => int.Parse(id));
@@ -80,9 +84,9 @@ namespace APPZ_new.Controllers
         }
 
         //from task grid
-        public async Task<ActionResult> ViewDetailedResult()
+        public async Task<ActionResult> ViewDetailedResult(int id)
         {
-            int userTaskId = 16;
+            int userTaskId = id;
             var userTask = _context.UserTasks
                               .Include(x => x.UserAnswers)
                               .ThenInclude(X => X.Question)
@@ -94,6 +98,54 @@ namespace APPZ_new.Controllers
             return View(userTask);
         }
 
+        public async Task<IActionResult> UsersTaskList()
+        {
+            string currentUserName = User.Identity.Name;
+            var userId = _context.Users.Where(q => q.Name == currentUserName).Select(p => p.Id).FirstOrDefault();
+            IEnumerable<UserTask> userTask = _context.UserTasks.Where(p => p.UserId == userId);
+
+            IEnumerable<Models.Task> tasks = _context.Tasks.Where(p => !userTask.Select(p => p.TaskId).Contains(p.Id));
+           
+            return View(tasks);
+        }
+
+        public async Task<IActionResult> UsersPassedTaskList()
+        {
+            string currentUserName = User.Identity.Name;
+            IEnumerable<UserTask> userTask = _context.UserTasks;
+            IEnumerable<Models.User> user = _context.Users;
+            IEnumerable<Models.Task> tasks = _context.Tasks;
+            var passedTask = tasks.Join(userTask, p => p.Id, q => q.TaskId,
+                (p, q) => new
+                {
+                    ID = p.Id,
+                    Title = p.Title,
+                    Description = p.Description,
+                    Severity = p.Severity,
+                    UserId = q.UserId,
+                    Status = q.Status,
+                    CategoryId = p.CategoryId
+                }).Join(user, p => p.UserId, q => q.Id, (p, q) => new
+            {
+                Id = p.ID,
+                Title = p.Title,
+                Description = p.Description,
+                Severity = p.Severity,
+                Status = p.Status,
+                UserName = q.Name,
+                CategoryId = p.CategoryId
+            }).Where(p=> p.UserName == currentUserName).Select(p => new Models.Task
+            {
+                Id = p.Id,
+                Title = p.Title,
+                Severity = p.Severity,
+                CategoryId = p.CategoryId,
+                Description = p.Description
+            });
+
+
+            return View(passedTask);
+        }
         //private bool UserTaskExists(int id)
         //{
         //    return _context.UserTasks.Any(e => e.Id == id);
