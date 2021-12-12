bdcd4b82a0eb342bb263c55927da2aa635e373d6
valeriiapav <lonome97@gmail.com>
2021-12-13 01:54:41 +0200

    HOTFIX

--- APPZ_new/Startup.cs
+++ APPZ_new/Startup.cs
@@ -36,8 +36,6 @@ namespace APPZ_new
                 options.UseSqlServer(
                     Configuration.GetConnectionString("MainDbConnection")), ServiceLifetime.Transient);
             services.AddControllersWithViews();
-            services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
